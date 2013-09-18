530e172fbd1d77fad33c11bd9ac538c428f0c9ec
Jonathan Beller <beller.jonathan@gmail.com>
2013-09-18 18:17:59 -0400

    Functions -> objects

--- dice.js
+++ dice.js
@@ -1,42 +1,44 @@
-function Die() {
+var Die = {
 
-    this.sideNames = {
+    sideNames : {
         NINE  : 0,
         TEN   : 1,
         JACK  : 2,
         QUEEN : 3,
         KING  : 4,
         ACE   : 5
-    };
+    },
 
-    this.sides = [this.sideNames.NINE,
-             this.sideNames.TEN,
-             this.sideNames.JACK,
-             this.sideNames.QUEEN,
-             this.sideNames.KING,
-             this.sideNames.ACE];
+    getSides : function() {
+        return [this.sideNames.NINE,
+                this.sideNames.TEN,
+                this.sideNames.JACK,
+                this.sideNames.QUEEN,
+                this.sideNames.KING,
+                this.sideNames.ACE];
+    },
 
-    this.getRandomSide = function() {
+    getRandomSide : function() {
         return Math.floor(Math.random() * 6);
-    };
+    },
 
-    this.getDie = function(sideFacingUp, hasBeenRolledThisTurn, isUnderCup) {
+    getDie : function(sideFacingUp, hasBeenRolledThisTurn, isUnderCup) {
         hasBeenRolledThisTurn = typeof hasBeenRolledThisTurn !== 'undefined' ? hasBeenRolledThisTurn : true;
         isUnderCup = typeof isUnderCup !== 'undefined' ? isUnderCup : true;
         return {sideFacingUp : sideFacingUp,
                 hasBeenRolledThisTurn : hasBeenRolledThisTurn,
                 isUnderCup : isUnderCup};
-    };
+    },
 
-    this.getRolledDie = function(hasBeenRolledThisTurn, isUnderCup) {
+    getRolledDie : function(hasBeenRolledThisTurn, isUnderCup) {
         hasBeenRolledThisTurn = typeof hasBeenRolledThisTurn !== 'undefined' ? hasBeenRolledThisTurn : true;
         isUnderCup = typeof isUnderCup !== 'undefined' ? isUnderCup : true;
         return this.getDie(this.getRandomSide(), hasBeenRolledThisTurn, isUnderCup);
-    };
+    }
 }
 
-function Hand() {
-    this.handTypes = {
+var Hand = {
+    handTypes : {
         NOTHING         : 0,
         PAIR            : 1,
         TWO_PAIR        : 2,
@@ -44,33 +46,31 @@ function Hand() {
         FULL_HOUSE      : 4,
         FOUR_OF_A_KIND  : 5,
         FIVE_OF_A_KIND  : 6
-    };
+    },
 
-    this.die = new Die();
-
-    this.getHand = function(dieList) {
+    getHand : function(dieList) {
         var hand = [];
         if (dieList) {
             for (var i = 0; i < 5 && i < dieList.length; i++) {
-                hand.push(this.die.getDie(dieList[i]));
+                hand.push(Die.getDie(dieList[i]));
             }
-            var diceToAdd = this.die.sides.sort().reverse().filter(function(side) { return hand.indexOf(side) == -1; });
+            var diceToAdd = Die.getSides().sort().reverse().filter(function(side) { return hand.indexOf(side) == -1; });
             while (hand.length < 5) {
-                hand.push(this.die.getDie(diceToAdd.pop()));
+                hand.push(Die.getDie(diceToAdd.pop()));
             }
         } else {
             for (var i = 0; i < 5; i++) {
-                hand.push(this.die.getRolledDie());
+                hand.push(Die.getRolledDie());
             }
         }
         return hand;
-    };
+    },
 
-    this.getLowestHand = function() {
-        return this.getHand([this.die.sideNames.TEN, this.die.sideNames.JACK, this.die.sideNames.QUEEN, this.die.sideNames.KING, this.die.sideNames.ACE]);
-    };
+    getLowestHand : function() {
+        return this.getHand([Die.sideNames.TEN, Die.sideNames.JACK, Die.sideNames.QUEEN, Die.sideNames.KING, Die.sideNames.ACE]);
+    },
 
-    this.getScore = function(hand) {
+    getScore : function(hand) {
         if (this.kind(hand, 5)) {
             return [this.handTypes.FIVE_OF_A_KIND, this.kind(hand, 5)]; 
         } else if (this.kind(hand, 4)) {
@@ -80,7 +80,7 @@ function Hand() {
         } else if (this.kind(hand, 3)) {
             return [this.handTypes.THREE_OF_A_KIND, this.kind(hand, 3), Math.max.apply(Math, this.kind(hand, 1)), Math.min.apply(Math, this.kind(hand, 1))];
         } else if (Object.prototype.toString.call(this.kind(hand, 2)) === '[object Array]') {
-            return [this.handTypes.TWO_PAIR, Math.max.apply(Math, this.kind(hand, 2)), Math.max.apply(Math, this.kind(hand, 2))];
+            return [this.handTypes.TWO_PAIR, Math.max.apply(Math, this.kind(hand, 2)), Math.min.apply(Math, this.kind(hand, 2))];
         } else if (this.kind(hand, 2)) {
             return [this.handTypes.PAIR, this.kind(hand, 2)];
         } else {
@@ -88,37 +88,66 @@ function Hand() {
         }
     },
 
-    this.kind = function(hand, num) {
+    kind : function(hand, num) {
         var matches = [];
-        for (var i = 0; i < this.die.sides.length; i++) {
-            handWithOneSideOnly = hand.filter(function(dieInHand) { return dieInHand.sideFacingUp == this.die.sides[i] });
+        for (var i = 0; i < Die.getSides().length; i++) {
+            handWithOneSideOnly = hand.filter(function(dieInHand) { return dieInHand.sideFacingUp == Die.getSides()[i] });
             if (handWithOneSideOnly.length == num) {
-                matches.push(this.die.sides[i]);
+                matches.push(Die.getSides()[i]);
             }
         }
         if (matches.length == 1) {
             return matches[0];
-        } else if (!matches) {
+        } else if (matches.length == 0) {
             return false;
         } else {
             return matches;
         }
     },
 
-    this.reroll = function(hand, dieNum) {
+    reroll : function(hand, dieNum) {
         var newHand = hand.slice(0);
-        newHand[dieNum] = this.die.getRolledDie(true, hand[dieNum].isUnderCup);
+        newHand[dieNum] = Die.getRolledDie(true, hand[dieNum].isUnderCup);
         return newHand;
     },
 
-    this.resetRolls = function(hand) {
+    resetRolls : function(hand) {
         var newHand = [];
         for (var i = 0; i < hand.length; i++) {
-            newHand.append(this.die.getDie(hand[i].sideFacingUp, false, hand[i].isUnderCup));
+            newHand.append(Die.getDie(hand[i].sideFacingUp, false, hand[i].isUnderCup));
         }
         return newHand;
+    },
+}
+
+var HandTextView = {
+    sideNames : ['9', '10', 'J', 'Q', 'K', 'A'],
+
+    getDescription : function(hand) {
+        if (Hand.kind(hand, 5)) {
+            return 'Five ' + this.sideNames[Hand.kind(hand, 5)] + 's';
+        } else if (Hand.kind(hand, 4)) {
+            if (Hand.kind(hand, 1) == Die.sideNames.ACE) {
+                return 'Four ' + this.sideNames[Hand.kind(hand, 4)] + 's and an ' + this.sideNames[Hand.kind(hand, 1)];
+            } else {
+                return 'Four ' + this.sideNames[Hand.kind(hand, 4)] + 's and a ' + this.sideNames[Hand.kind(hand, 1)];
+            }
+        } else if (Hand.kind(hand, 3) && Hand.kind(hand, 2)) {
+            return this.sideNames[Hand.kind(hand, 3)] + 's full of ' + this.sideNames[Hand.kind(hand, 2)] + 's';
+        } else if (Hand.kind(hand, 3)) {
+            if (Math.max.apply(Math, Hand.kind(hand, 1)) == Die.sideNames.ACE) {
+                return 'Three ' + this.sideNames[Hand.kind(hand, 3)] + 's and an ' + this.sideNames[Math.max.apply(Math, Hand.kind(hand, 1))];
+            } else {
+                return 'Three ' + this.sideNames[Hand.kind(hand, 3)] + 's and a ' + this.sideNames[Hand.kind(hand, 1)];
+            }
+        } else if (Object.prototype.toString.call(Hand.kind(hand, 2)) === '[object Array]') {
+            return this.sideNames[Math.max.apply(Math, Hand.kind(hand, 2))] + 's and ' + this.sideNames[Math.min.apply(Math, Hand.kind(hand, 2))] + 's';
+        } else if (Hand.kind(hand, 2)) {
+            return 'Pair of ' + this.sideNames[Hand.kind(hand, 2)] + 's';
+        } else {
+            return 'Nothing';
+        }
     }
 
 }
-
 
