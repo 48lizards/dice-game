a73d70e8e26e75ed391f81d0b7319ebf4f664958
Jonathan Beller <beller.jonathan@gmail.com>
2013-09-23 12:47:44 -0400

    Make dice draggable

--- dice-game.js
+++ dice-game.js
@@ -1,15 +1,17 @@
-var hand = Hand.getHand();
+var hand = Hand.makeHand();
 for (var i = 0; i < 5; i++) {
     $('#dice-container').append('<div class="die" id="die' + i + '">' + HandTextView.sideNames[hand[i].sideFacingUp]);
 }
 $('#handname-display').text(HandTextView.getDescription(hand));
+$('.die').draggable();
 $('#dice-container').children().click(function() {            
-hand = Hand.reroll(hand, $(this).index());
-$(this).text(HandTextView.sideNames[hand[$(this).index()].sideFacingUp]);
-$('#handname-display').text(HandTextView.getDescription(hand));
+    hand = Hand.reroll(hand, $(this).index());
+    $(this).text(HandTextView.sideNames[hand[$(this).index()].sideFacingUp]);
+    $('#handname-display').text(HandTextView.getDescription(hand));
 });
 
 $('#pass').click(function(){
     hand = Hand.resetRolls(hand);
 });
+
 
